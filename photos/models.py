3053a5c6c9f340a5e373154cc849a21df03e0554
Dave <david.mathaga@student.moringaschool.com>
2021-12-01 00:16:10 +0300

    test

--- photos/models.py
+++ photos/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import datetime as dt
 
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100,null=False,blank=False)
