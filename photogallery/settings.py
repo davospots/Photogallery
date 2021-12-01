f588c6ef0b7f4e1e909bc15361572457a32b78fc
Dave <david.mathaga@student.moringaschool.com>
2021-12-01 17:30:32 +0300

    req

--- photogallery/settings.py
+++ photogallery/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'photos.apps.PhotosConfig',
     
     
-   
+    'whitenoise.runserver_nostatic'
 
     
 ]
@@ -58,7 +58,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     
     'django.middleware.security.SecurityMiddleware',
-    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
