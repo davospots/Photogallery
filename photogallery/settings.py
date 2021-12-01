7e24d5f5b15a2de90e36d02ceb4f8ede753b23fd
Dave <david.mathaga@student.moringaschool.com>
2021-12-01 17:22:41 +0300

    req

--- photogallery/settings.py
+++ photogallery/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'photos.apps.PhotosConfig',
-    'bootstrap5',
+    
     
     'whitenoise.runserver_nostatic'
 
