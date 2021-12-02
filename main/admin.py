a919be8cc5807a56ccf1c0189c5e00a65fd5be2d
Dave <david.mathaga@student.moringaschool.com>
2021-12-02 09:38:11 +0300

    debugging

--- main/admin.py
+++ main/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 class AlbumAdmin(admin.ModelAdmin):
-	list_display=('title','image_tag','user','status')
+	list_display=('title','image_tag','status')
 admin.site.register(models.Album,AlbumAdmin)
 
 class PhotosAdmin(admin.ModelAdmin):
--- photogallery/settings.py
+++ photogallery/settings.py
@@ -122,7 +122,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIR = [
+    BASE_DIR / "static",
+    
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
