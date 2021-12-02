7baa0c30cb64e8754488c8a295b9a63e7a0a7d48
Dave <david.mathaga@student.moringaschool.com>
2021-12-02 09:47:53 +0300

    debugging

--- photogallery/settings.py
+++ photogallery/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5e1@k_5gd_3xqi)kp)*nt0#t+ijx(f$6v$&7%5@q!c+w80ly#5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'photodavos.herokuapp.com/']
 
